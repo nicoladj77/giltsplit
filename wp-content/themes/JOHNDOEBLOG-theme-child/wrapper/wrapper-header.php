4b023d052bdd0e8c26cdb7bfee9b017d4125b29f
Nicola Peluchetti <nicola.peluchetti@gmail.com>
2014-02-25 13:23:10 +0100

    modded

---
+++ wp-content/themes/JOHNDOEBLOG-theme-child/wrapper/wrapper-header.php
@@ -0,0 +1,19 @@
+<?php /* Wrapper Name: Header */ ?>
+<?php
+global $hercules_add_animatedheader;
+$hercules_add_animatedheader = false;
+	?>
+
+
+<div class="row-fluid">
+
+	</div>
+	<div class="row-fluid post-header">
+	<div class="span12">
+    	<?php get_template_part("static/static-nav"); ?>
+    </div>
+
+</div>
+<!--<div class="row"> -->
+    
+<!-- </div> -->
